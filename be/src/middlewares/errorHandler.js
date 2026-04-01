128b843a5a1da3f534e7f6c98d32b3a4f16ee7c7
k_sih <160022893+Cosmosirius@users.noreply.github.com>
2026-04-01 14:47:31 +0700

    hapus folder be

--- be/src/middlewares/errorHandler.js
+++
@@ -1,7 +0,0 @@
-module.exports = (err, req, res, next) => {
-  console.error(err);
-  res.status(err.status || 500).json({
-    success: false,
-    message: err.message || 'Terjadi kesalahan pada server',
-  });
-};
--- be/src/modules/inventory/inventory.controller.js
+++
@@ -1,6 +0,0 @@
-const s = require('./inventory.service');
-
-const getMutasi   = async (req, res, next) => { try { res.json({ success: true, data: await s.getMutasi(req.user.tokoId, req.query) }); } catch (e) { next(e); } };
-const tambahStok  = async (req, res, next) => { try { res.status(201).json({ success: true, data: await s.tambahStok(req.user.tokoId, req.user.id, req.body) }); } catch (e) { next(e); } };
-
-module.exports = { getMutasi, tambahStok };
